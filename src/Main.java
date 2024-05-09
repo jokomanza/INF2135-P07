0b25cd8dda9a41deb6ab826fa30ece6f335b2f70
Joko Supriyanto <jokomanza@gmail.com>
2024-05-09 10:12:58 +0700

    Update sample objects in main method

--- src/Main.java
+++ src/Main.java
@@ -13,41 +13,103 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Program Administrasi Penerbangan\n");
 
-        Penerbangan penerbangan = generatePenerbangan();
-        printPenerbangan(penerbangan);
-    }
+        Pilot pilot1 = new Pilot("PL001", "John Doe");
+        Pilot pilot2 = new Pilot("PL002", "Jane Smith");
+        Pilot pilot3 = new Pilot("PL003", "Robert Jonshon");
+        Pilot pilot4 = new Pilot("PL004", "Michael Jackson");
 
-    private static Penerbangan generatePenerbangan() {
-        Pilot pilot1 = new Pilot("P001", "John Doe");
-        Pilot pilot2 = new Pilot("P002", "Jane Doe");
+        Teknisi teknisi1 = new Teknisi("TK001", "Michael Miller");
+        Teknisi teknisi2 = new Teknisi("TK002", "William Wilson");
+        Teknisi teknisi3 = new Teknisi("TK003", "David Davis");
+        Teknisi teknisi4 = new Teknisi("TK004", "Richard Richardson");
 
-        Pramugari pramugari1 = new Pramugari("P001", "John Doe");
-        Pramugari pramugari2 = new Pramugari("P002", "Jane Doe");
+        Pramugari pramugari1 = new Pramugari("PG001", "Maria Garcia");
+        Pramugari pramugari2 = new Pramugari("PG002", "Anna Anderson");
+        Pramugari pramugari3 = new Pramugari("PG003", "Jennifer James");
+        Pramugari pramugari4 = new Pramugari("PG004", "Linda Lopez");
 
-        Teknisi teknisi1 = new Teknisi("T001", "John Doe");
-        Teknisi teknisi2 = new Teknisi("T002", "Jane Doe");
+        Airline garudaIndonesia = new Airline("Garuda Indonesia");
+        garudaIndonesia.setKodeIATA("GA");
+        garudaIndonesia.setKodeICAO("GIA");
+        garudaIndonesia.setNegara("Indonesia");
 
-        Airline airline = new Airline("Garuda Indonesia");
-        airline.setKodeIATA("GA");
-        airline.setKodeICAO("GIA");
-        airline.setNegara("Indonesia");
+        Airline singaporeAirlines = new Airline("Singapore Airlines");
+        singaporeAirlines.setKodeIATA("SQ");
+        singaporeAirlines.setKodeICAO("SIA");
+        singaporeAirlines.setNegara("Singapore");
 
-        Pesawat pesawat = new Pesawat("PK-GIA", "Boeing 737-800", "Boeing", 189);
+        Airline cathayPacific = new Airline("Cathay Pacific");
+        cathayPacific.setKodeIATA("CX");
+        cathayPacific.setKodeICAO("CPA");
+        cathayPacific.setNegara("Hong Kong");
 
-        Airport airportAsal = new Airport("Soekarno-Hatta International Airport", "CGK", "WIII", "Tangerang");
-        Airport airportTujuan = new Airport("Changi International Airport", "SIN", "WSSS", "Singapore");
+        Airline qatarAirways = new Airline("Qatar Airways");
+        qatarAirways.setKodeIATA("QR");
+        qatarAirways.setKodeICAO("QTR");
+        qatarAirways.setNegara("Qatar");
 
-        Penerbangan penerbangan = new Penerbangan("GA123");
-        penerbangan.setAirline(airline);
-        penerbangan.setPesawat(pesawat);
-        penerbangan.setPilots(List.of(pilot1, pilot2));
-        penerbangan.setPramugari(pramugari1);
-        penerbangan.setTeknisi(teknisi1);
-        penerbangan.setAirportAsal(airportAsal);
-        penerbangan.setAirportTujuan(airportTujuan);
-        penerbangan.setJumlahPenumpang(189);
+        Pesawat boeing737 = new Pesawat("PK-GIA", "Boeing 737-800", "Boeing", 189);
+        Pesawat airbusA350 = new Pesawat("PK-SIA", "Airbus A350-900", "Airbus", 303);
+        Pesawat boeing777 = new Pesawat("PK-CPA", "Boeing 777-300ER", "Boeing", 368);
+        Pesawat airbusA380 = new Pesawat("PK-QTR", "Airbus A380-800", "Airbus", 517);
 
-        return penerbangan;
+        Airport soekarnoHatta = new Airport("Soekarno-Hatta International Airport", "CGK", "WIII", "Tangerang");
+        Airport changiAirport = new Airport("Changi International Airport", "SIN", "WSSS", "Singapore");
+        Airport hongKongInternational = new Airport("Hong Kong International Airport", "HKG", "VHHH", "Hong Kong");
+        Airport hamadInternational = new Airport("Hamad International Airport", "DOH", "OTHH", "Doha");
+
+        Penerbangan penerbangan1 = new Penerbangan("GA123");
+        penerbangan1.setAirline(garudaIndonesia);
+        penerbangan1.setPesawat(boeing737);
+        penerbangan1.setPilots(List.of(pilot1, pilot2));
+        penerbangan1.setPramugari(pramugari1);
+        penerbangan1.setTeknisi(teknisi1);
+        penerbangan1.setAirportAsal(soekarnoHatta);
+        penerbangan1.setAirportTujuan(changiAirport);
+        penerbangan1.setJumlahPenumpang(189);
+
+
+        Penerbangan penerbangan2 = new Penerbangan("SQ456");
+        penerbangan2.setAirline(singaporeAirlines);
+        penerbangan2.setPesawat(airbusA350);
+        penerbangan2.setPilots(List.of(pilot3, pilot4));
+        penerbangan2.setPramugari(pramugari2);
+        penerbangan2.setTeknisi(teknisi2);
+        penerbangan2.setAirportAsal(changiAirport);
+        penerbangan2.setAirportTujuan(hongKongInternational);
+        penerbangan2.setJumlahPenumpang(303);
+
+        Penerbangan penerbangan3 = new Penerbangan("CX789");
+        penerbangan3.setAirline(cathayPacific);
+        penerbangan3.setPesawat(boeing777);
+        penerbangan3.setPilots(List.of(pilot1, pilot3));
+        penerbangan3.setPramugari(pramugari3);
+        penerbangan3.setTeknisi(teknisi3);
+        penerbangan3.setAirportAsal(hongKongInternational);
+        penerbangan3.setAirportTujuan(hamadInternational);
+        penerbangan3.setJumlahPenumpang(368);
+
+        Penerbangan penerbangan4 = new Penerbangan("QR012");
+        penerbangan4.setAirline(qatarAirways);
+        penerbangan4.setPesawat(airbusA380);
+        penerbangan4.setPilots(List.of(pilot2, pilot4));
+        penerbangan4.setPramugari(pramugari4);
+        penerbangan4.setTeknisi(teknisi4);
+        penerbangan4.setAirportAsal(hamadInternational);
+        penerbangan4.setAirportTujuan(soekarnoHatta);
+        penerbangan4.setJumlahPenumpang(517);
+
+        printPenerbangan(penerbangan1);
+        System.out.println();
+
+        printPenerbangan(penerbangan2);
+        System.out.println();
+
+        printPenerbangan(penerbangan3);
+        System.out.println();
+
+        printPenerbangan(penerbangan4);
+        System.out.println();
     }
 
     private static void printPenerbangan(Penerbangan penerbangan) {
